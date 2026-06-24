5e49add485c5a9fe95cec71c9023ad748e1724e5
RabsonJ <phirirabsonj@gmail.com>
2026-06-24 08:23:05 +0200

    feat: add backend CI workflow with testing and coverage setup

---
+++ backend/config/test_settings.py
@@ -0,0 +1,13 @@
+import os
+from config.settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_appli_tracker",
+        "USER": os.environ.get("POSTGRES_USER", "appli_tracker"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+    }
+}
